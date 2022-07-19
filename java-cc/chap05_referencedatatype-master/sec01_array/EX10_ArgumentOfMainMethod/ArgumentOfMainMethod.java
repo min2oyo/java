6eb9b19afedfb48c73d4ed83c2e7a46b09dad9c5
min2oyo <min2u@outlook.com>
2022-07-19 21:32:32 +0900

    chap05 정리

--- java-cc/chap05_referencedatatype-master/sec01_array/EX10_ArgumentOfMainMethod/ArgumentOfMainMethod.java
+++ java-cc/chap05_referencedatatype-master/sec01_array/EX10_ArgumentOfMainMethod/ArgumentOfMainMethod.java
@@ -1,24 +1,35 @@
 package sec01_array.EX10_ArgumentOfMainMethod;
 
 /*main 메서드로의 매개변수 전달*/
+/*
+ * Run - Run configurations - Java Application - 'ArgumentOfMainMethod' - '(x)=Arguments' 탭
+ * '안녕하세요 3 5.8' 입력
+ */
 
 public class ArgumentOfMainMethod {
-	public static void main(String[] args) {		
-		//#1. args 배열의 길이 구하기
+
+	public static void main(String[] args) {
+
+		// #1. args 배열의 길이 구하기
+
 		System.out.println(args.length);
 		System.out.println();
-		
-		//#2. 매개변수 출력 1
-		for(int i=0; i<args.length; i++) {
+
+		// #2. 매개변수 출력 1
+
+		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
-		//System.out.println(Integer.parseInt(args[1])+1); //4??? 31
-		System.out.println();		
-		
-		//#3. 매개변수 출력 2
-		for(String s : args) {
+		System.out.println(Integer.parseInt(args[1]) + 1); // 4
+		System.out.println();
+
+		// #3. 매개변수 출력 2
+
+		for (String s : args) {
 			System.out.println(s);
 		}
-		System.out.println();		
+		System.out.println();
+
 	}
+
 }
