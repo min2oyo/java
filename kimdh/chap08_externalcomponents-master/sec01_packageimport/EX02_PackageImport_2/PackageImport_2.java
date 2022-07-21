70d8fddb97a1d69f7c719c901257a083624a37a6
min2oyo <min2u@outlook.com>
2022-07-21 11:21:55 +0900

    chap08 완강

--- kimdh/chap08_externalcomponents-master/sec01_packageimport/EX02_PackageImport_2/PackageImport_2.java
+++ kimdh/chap08_externalcomponents-master/sec01_packageimport/EX02_PackageImport_2/PackageImport_2.java
@@ -9,9 +9,11 @@ public class PackageImport_2 {
 	public static void main(String[] args) {
 
 		// #1. 객체생성
+
 		A a = new A(); // 정상동작
 
 		// #2. 멤버활용
+
 		System.out.println(a.m); // 3
 		System.out.println(a.n); // 4
 		a.print(); // 임포트
