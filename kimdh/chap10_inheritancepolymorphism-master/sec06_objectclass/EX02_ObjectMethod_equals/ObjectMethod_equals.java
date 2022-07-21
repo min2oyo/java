3ffec12f9fde1981eaebf5a753ada98464c632f8
min2oyo <min2u@outlook.com>
2022-07-21 21:04:59 +0900

    kimdh | chap10.6 완강

--- kimdh/chap10_inheritancepolymorphism-master/sec06_objectclass/EX02_ObjectMethod_equals/ObjectMethod_equals.java
+++ kimdh/chap10_inheritancepolymorphism-master/sec06_objectclass/EX02_ObjectMethod_equals/ObjectMethod_equals.java
@@ -1,58 +1,58 @@
 package sec06_objectclass.EX02_ObjectMethod_equals;
 
-/*Object 클래스의 equals(.) 메서드*/
+/* Object 클래스의 equals(.) 메서드 */
 
-class A{
+class A {
+
 	String name;
-	A(String name){
+
+	A(String name) {
+
 		this.name = name;
+
 	}
+
 }
-class B{
+
+class B {
+
 	String name;
-	B(String name){
+
+	B(String name) {
+
 		this.name = name;
+
 	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if(this.name == ((B)obj).name) {
+
+		if (this.name == ((B) obj).name) {
 			return true;
-		} else 
+		} else {
 			return false;
-	}	
+		}
+
+	}
+
 }
+
 public class ObjectMethod_equals {
-	public static void main(String[] args) {		
+
+	public static void main(String[] args) {
+
 		A a1 = new A("안녕");
 		A a2 = new A("안녕");
-		
-		System.out.println(a1==a2); //false
-		System.out.println(a1.equals(a2));//false
-		
-		
+
+		System.out.println(a1 == a2); // false
+		System.out.println(a1.equals(a2));// false
+
 		B b1 = new B("안녕");
 		B b2 = new B("안녕");
-		
-		System.out.println(b1==b2); //false
-		System.out.println(b1.equals(b2));//true		
+
+		System.out.println(b1 == b2); // false
+		System.out.println(b1.equals(b2)); // true
+
 	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
