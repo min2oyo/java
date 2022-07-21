76c4793b0e8c47f64d12befbf90564890f0dc8a0
min2oyo <min2u@outlook.com>
2022-07-21 17:00:36 +0900

    chap10.3 완강

--- kimdh/chap10_inheritancepolymorphism-master/sec03_methodoverriding/EX02_MethodOverriding_2/MethodOverriding_2.java
+++ kimdh/chap10_inheritancepolymorphism-master/sec03_methodoverriding/EX02_MethodOverriding_2/MethodOverriding_2.java
@@ -1,84 +1,82 @@
 package sec03_methodoverriding.EX02_MethodOverriding_2;
 
-/*메서드 오버라이딩의 대표적 예시*/
+/* 메서드 오버라이딩의 대표적 예시 */
 
 class Animal {
-	void cry() {}
+
+	void cry() {
+
+	}
+
 }
+
 class Bird extends Animal {
+
 	@Override
 	void cry() {
+
 		System.out.println("짹짹");
+
 	}
+
 }
+
 class Cat extends Animal {
+
 	@Override
 	void cry() {
+
 		System.out.println("야옹");
+
 	}
+
 }
+
 class Dog extends Animal {
+
 	@Override
 	void cry() {
+
 		System.out.println("멍멍");
+
 	}
+
 }
 
 public class MethodOverriding_2 {
-	public static void main(String[] args) {		
-		//#1. 각각의 타입으로 선언 + 각각의 타입으로 생성
+
+	public static void main(String[] args) {
+
+		// #1. 각각의 타입으로 선언 + 각각의 타입으로 생성
+
 		Animal aa = new Animal();
 		Bird bb = new Bird();
 		Cat cc = new Cat();
-		Dog dd = new Dog();		
-		aa.cry();
-		bb.cry();
-		cc.cry();
-		dd.cry();
+		Dog dd = new Dog();
+		aa.cry(); //
+		bb.cry(); // 짹짹
+		cc.cry(); // 야옹
+		dd.cry(); // 멍멍
 		System.out.println();
-		
-		//#2. Animal 타입으로 선언 + 자식클래스 타입으로 생성
+
+		// #2. Animal 타입으로 선언 + 자식클래스 타입으로 생성
+
 		Animal ab = new Bird();
 		Animal ac = new Cat();
-		Animal ad = new Dog();		
-		ab.cry();
-		ac.cry();
-		ad.cry();		
+		Animal ad = new Dog();
+		ab.cry(); // 짹짹
+		ac.cry(); // 야옹
+		ad.cry(); // 멍멍
 		System.out.println();
-		
-		//#3. 배열로 관리
-		Animal[] animals = {ab, ac, ad};
-		for(Animal animal : animals) {
-			animal.cry();
+
+		// #3. 배열로 관리
+
+		Animal[] animals = { ab, ac, ad };
+
+		for (Animal animal : animals) {
+			animal.cry(); // 짹짹 // 야옹 // 멍멍
 		}
+
 	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
