f4c906a43aced85118fa62d42b6fe120394caa86
min2oyo <min2u@outlook.com>
2022-07-21 17:32:37 +0900

    kimdh | chap10.4 완강

--- kimdh/chap10_inheritancepolymorphism-master/sec04_overlapmember/EX02_OverlapStaticField/OverlapStaticField.java
+++ kimdh/chap10_inheritancepolymorphism-master/sec04_overlapmember/EX02_OverlapStaticField/OverlapStaticField.java
@@ -1,28 +1,42 @@
 package sec04_overlapmember.EX02_OverlapStaticField;
 
-/*정적 필드의 중복*/
+/* 정적 필드의 중복 */
 
 class A {
+
 	static int m = 3;
+
 }
+
 class B extends A {
+
 	static int m = 4;
+
 }
+
+@SuppressWarnings("static-access")
 public class OverlapStaticField {
-	public static void main(String[] args) {		
-		//#1.클래스 이름으로 바로 접근
-		System.out.println(A.m); //3
-		System.out.println(B.m); //4
+
+	public static void main(String[] args) {
+
+		// #1.클래스 이름으로 바로 접근
+
+		System.out.println(A.m); // 3
+		System.out.println(B.m); // 4
 		System.out.println();
-		
-		//#2.객체 생성
+
+		// #2.객체 생성
+
 		A aa = new A();
 		B bb = new B();
 		A ab = new B();
-		
-		//#3. 객체 생성을 통한 static field
-		System.out.println(aa.m); //3		
-		System.out.println(bb.m); //4
-		System.out.println(ab.m); //3
+
+		// #3. 객체 생성을 통한 static field
+
+		System.out.println(aa.m); // 3
+		System.out.println(bb.m); // 4
+		System.out.println(ab.m); // 3
+
 	}
+
 }
