175824a6c099ec444a09983f1b2e3fbe2d30472f
min2oyo <min2u@outlook.com>
2022-07-21 12:46:31 +0900

    chap09.1 완강

--- kimdh/chap09_modifier_1-master/sec01_accessmodifier/EX03_AccessModifierOfClass_2/pack/CC.java
+++ kimdh/chap09_modifier_1-master/sec01_accessmodifier/EX03_AccessModifierOfClass_2/pack/CC.java
@@ -1,10 +1,12 @@
 package sec01_accessmodifier.EX03_AccessModifierOfClass_2.pack;
 
-/*다른 패키지에서의 클래스 접근지정자 vs. 생성자 접근지정자*/
+/* 다른 패키지에서의 클래스 접근지정자 vs. 생성자 접근지정자 */
 
-//public class
+// public class
 public class CC {
-	CC(){ //default 생성자
-		
+
+	CC() { // default 생성자
+
 	}
+
 }
