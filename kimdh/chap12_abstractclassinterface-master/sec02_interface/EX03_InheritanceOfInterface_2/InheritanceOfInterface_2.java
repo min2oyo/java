a92bd3ad88e3ffaf205294032b179ac7271960b4
min2oyo <min2u@outlook.com>
2022-07-22 16:48:03 +0900

    kimdh | chap12 완강

--- kimdh/chap12_abstractclassinterface-master/sec02_interface/EX03_InheritanceOfInterface_2/InheritanceOfInterface_2.java
+++ kimdh/chap12_abstractclassinterface-master/sec02_interface/EX03_InheritanceOfInterface_2/InheritanceOfInterface_2.java
@@ -1,27 +1,43 @@
 package sec02_interface.EX03_InheritanceOfInterface_2;
 
-/*인터페이스 상속 자식클래스의 접근지정자*/
+/* 인터페이스 상속 자식클래스의 접근지정자 */
 
 interface A {
-	public abstract void abc(); 
+
+	public abstract void abc();
+
 }
+
 interface B {
-	void bcd(); //생략시 public abstract 
+
+	void bcd(); // 생략시 public abstract
+
 }
+
 class C implements A {
+
+	@Override
 	public void abc() {
-		//...
+
+		// ...
+
 	}
+
 }
-/* public -> default 불가능
-class D implements B {	
-	void bcd() {
-		
-	}	
-}
-*/
+
+//// public -> default 불가능 
+//class D implements B {
+//
+//	void bcd() {
+//
+//	}
+//
+//}
+
 public class InheritanceOfInterface_2 {
+
 	public static void main(String[] args) {
-		
+
 	}
+
 }
--- kimdh/chap12_abstractclassinterface-master/sec02_interface/EX04_CreateObjectOfInterface_1/CreateObjectOfInterface_1.java
+++ kimdh/chap12_abstractclassinterface-master/sec02_interface/EX04_CreateObjectOfInterface_1/CreateObjectOfInterface_1.java
@@ -1,24 +1,40 @@
 package sec02_interface.EX04_CreateObjectOfInterface_1;
 
-/*자식클래스의 직접 정의를 통한 인터페이스타입의 객체 생성 (방법#1)*/
+/* 자식 클래스의 직접 정의를 통한 인터페이스타입의 객체 생성 (방법#1) */
 
 interface A {
+
 	int a = 3;
+
 	void abc();
+
 }
-class B implements A {	
+
+class B implements A {
+
+	@Override
 	public void abc() {
-		System.out.println("방법#1. 자식클래스 생성자로 객체 생성");		
+
+		System.out.println("방법#1. 자식 클래스 생성자로 객체 생성");
+
 	}
+
 }
+
 public class CreateObjectOfInterface_1 {
+
 	public static void main(String[] args) {
-		//#1. 객체 생성
+
+		// #1. 객체 생성
+
 		A b1 = new B();
 		A b2 = new B();
-		
-		//#2. 메서드 호출
-		b1.abc();
-		b2.abc();
+
+		// #2. 메서드 호출
+
+		b1.abc(); // 방법#1. 자식 클래스 생성자로 객체 생성
+		b2.abc(); // 방법#1. 자식 클래스 생성자로 객체 생성
+
 	}
+
 }
