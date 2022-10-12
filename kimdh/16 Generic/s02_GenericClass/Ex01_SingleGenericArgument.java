d4506d7ffea1b5dc5a9964ecb058a9736222845e
min2oyo <min2u@outlook.com>
2022-10-12 10:52:13 +0900

    kimdh | 16.2 완강

--- kimdh/16 Generic/s02_GenericClass/Ex01_SingleGenericArgument.java
+++ kimdh/16 Generic/s02_GenericClass/Ex01_SingleGenericArgument.java
@@ -1,24 +1,37 @@
-package s02_genericclass.EX01_SingleGenericArgument;
+package s02_GenericClass;
 
 class MyClass<T> {
+
 	private T t;
+
 	public T get() {
+
 		return t;
+
 	}
+
 	public void set(T t) {
+
 		this.t = t;
-	}	
+
+	}
+
 }
 
-public class SingleGenericArgument {
+public class Ex01_SingleGenericArgument {
+
 	public static void main(String[] args) {
+
 		MyClass<String> mc1 = new MyClass<String>();
 		mc1.set("안녕");
 		System.out.println(mc1.get());
 		MyClass<Integer> mc2 = new MyClass<>();
 		mc2.set(100);
 		System.out.println(mc2.get());
+
 //		MyClass<Integer> mc3 = new MyClass<>();
-//		mc3.set("안녕"); //오류 강한타입체크
+//		mc3.set("안녕"); // 오류 강한타입체크
+
 	}
+
 }
