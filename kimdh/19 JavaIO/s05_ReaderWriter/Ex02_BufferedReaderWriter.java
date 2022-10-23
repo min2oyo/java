71f89e9c4cf6a655e162d7ba2d8f5ecee25be651
min2oyo <min2u@outlook.com>
2022-10-23 19:54:00 +0900

    kimdh | 19.3 완강

--- kimdh/19 JavaIO/s05_ReaderWriter/Ex02_BufferedReaderWriter.java
+++ kimdh/19 JavaIO/s05_ReaderWriter/Ex02_BufferedReaderWriter.java
@@ -10,33 +10,41 @@ import java.io.Reader;
 import java.io.Writer;
 
 public class Ex02_BufferedReaderWriter {
+
 	public static void main(String[] args) {
-		
-		//#파일객체 생성
-		File readerWriterFile = new File("src/sec05_readerwriter/files/BufferedFile.txt");
-		
-		//#1.FileWiter를 이용한 파일 쓰기 (UTF-8 모드)
-		try(Writer writer = new FileWriter(readerWriterFile);
-			BufferedWriter bw = new BufferedWriter(writer);	){
-			
+
+		// 파일객체 생성
+		File readerWriterFile = new File("files/BufferedFile.txt");
+
+		// 1.FileWiter를 이용한 파일 쓰기 (UTF-8 모드)
+		try (Writer writer = new FileWriter(readerWriterFile); BufferedWriter bw = new BufferedWriter(writer);) {
+
 			bw.write("안녕하세요\n".toCharArray());
 			bw.write("Hello");
 			bw.write('\r');
 			bw.write('\n');
 			bw.write("반갑습니다.", 2, 3);
 			bw.flush();
-			
-		}catch(IOException e) {}
-		
-		//#2.FileReader를 이용한 파일 읽기 (UTF-8 모드)
-		try(Reader reader = new FileReader(readerWriterFile);
-			BufferedReader br = new BufferedReader(reader);){
-			
+
+		} catch (IOException e) {
+
+		}
+
+		// 2.FileReader를 이용한 파일 읽기 (UTF-8 모드)
+		try (Reader reader = new FileReader(readerWriterFile); BufferedReader br = new BufferedReader(reader);) {
+
 			String data;
-			while((data=br.readLine())!=null) {
+
+			while ((data = br.readLine()) != null) {
+
 				System.out.println(data);
+
 			}
-			
-		} catch(IOException e) {}
+
+		} catch (IOException e) {
+
+		}
+
 	}
+
 }
